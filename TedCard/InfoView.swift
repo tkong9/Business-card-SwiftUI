6880d8bfab1f131e869b565755343139f69abf94
tkong9 <taewon2711@gmail.com>
2020-01-27 01:58:00 -0500

    add readme

---
+++ TedCard/InfoView.swift
@@ -0,0 +1,21 @@
+//
+//  InfoView.swift
+//  TedCard
+//
+//  Created by TAEWON KONG on 10/10/19.
+//  Copyright © 2019 TAEWON KONG. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
